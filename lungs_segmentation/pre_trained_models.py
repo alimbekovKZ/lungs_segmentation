c987c47948627792f4802c60979e7fcc946e8ecb
RENAT ALIMBEKOV <alimbekovr@hotmail.com>
2021-01-16 13:00:31 +0600

    init commit

---
+++ lungs_segmentation/pre_trained_models.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+from torch import nn
+from torch.utils import model_zoo
+
+import models.selim_zoo.unet as models
+
+model = namedtuple("model", ["url", "model"])
+
+models = {
+    "resnet34": model(
+        url="https://github.com/alimbekovKZ/lungs_segmentation/releases/download/1.0.0/resnet34.pth",
+        model=models.Resnet(seg_classes=2, backbone_arch='resnet34'),
+    ),
+    "densenet121": model(
+        url="https://github.com/alimbekovKZ/lungs_segmentation/releases/download/1.0.0/densenet121.pth",
+        model=models.DensenetUnet(seg_classes=2, backbone_arch='densenet121'),
+    )
+}
+
+def create_model(model_name: str) -> nn.Module:
+    model = models[model_name].model
+    state_dict = model_zoo.load_url(models[model_name].url, progress=True, map_location="cpu")#["state_dict"]
+    model.load_state_dict(state_dict)
+    return model
